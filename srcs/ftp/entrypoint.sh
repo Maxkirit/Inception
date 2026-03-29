16f886e498cdac877f193f1229360599ce9f7776
Maxkirit <maximeturgeon@ymail.com>
2026-03-29 18:00:01 +0200

    fix: changed folder creation of ftp to entrypoint

--- srcs/ftp/entrypoint.sh
+++ srcs/ftp/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
+mkdir -p /var/www/html/ftp
+chown root:root /var/www/html/ftp
+chmod 755 /var/www/html/ftp
 echo "ftp:$(cat /run/secrets/ftp_password)" | chpasswd
 echo "ftp" > /etc/vsftpd/user_list
 exec vsftpd /etc/vsftpd/vsftpd.conf
