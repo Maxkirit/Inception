11cb8d0a5b81d7b383cf1abd1c37f38316cf0506
Maxkirit <maximeturgeon@ymail.com>
2026-03-29 18:50:03 +0200

    fix: added seccomp_sandbox=no and upload directory

--- srcs/ftp/entrypoint.sh
+++ srcs/ftp/entrypoint.sh
@@ -3,6 +3,12 @@ set -e
 mkdir -p /var/www/html/ftp
 chown root:root /var/www/html/ftp
 chmod 755 /var/www/html/ftp
+
+#main upload dir
+mkdir -p /var/www/html/ftp/uploads
+chown ftp:ftp /var/www/html/ftp/uploads
+chmod 755 /var/www/html/ftp/uploads
+
 echo "ftp:$(cat /run/secrets/ftp_password)" | chpasswd
 echo "ftp" > /etc/vsftpd/user_list
 exec vsftpd /etc/vsftpd/vsftpd.conf
