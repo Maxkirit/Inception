557ed4676d2ef561828ae866275e7f93e8537427
Maxkirit <maximeturgeon@ymail.com>
2026-03-30 15:25:49 +0200

    fix: new zola entrypoint and nginx fix

--- srcs/zola_website/entrypoint.sh
+++ srcs/zola_website/entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 cd /project
-zola build --base-url $ZOLA_URL --output-dir /tmp/zola_output --force
-cp /tmp/zola_output/* /data/.
-rm -rf /tmp/zola_output
+zola build --base-url "$ZOLA_URL" --output-dir "$ZOLA_DIR" --force
+# cp -r /tmp/zola_output/* /data/
+# rm -rf /tmp/zola_output
